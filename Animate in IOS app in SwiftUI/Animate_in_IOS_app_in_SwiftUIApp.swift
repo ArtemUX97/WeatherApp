c7d21ad06e16f4bb561ba52ab3b4a771ad2abe0f
Artem Isaak <izaa.artem@yandex.ru.com>
2023-04-07 20:59:29 +0200

    Initial Commit

---
+++ Animate in IOS app in SwiftUI/Animate_in_IOS_app_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animate_in_IOS_app_in_SwiftUIApp.swift
+//  Animate in IOS app in SwiftUI
+//
+//  Created by Артем Изаак on 07.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Animate_in_IOS_app_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
